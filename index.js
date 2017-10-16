cf8e51cb87ee8c806e72727b4dc49faffb91e362
Stephanie Baron <stephaniebaron12@gmail.com>
2017-10-16 11:57:01 -0400

    Added Express

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const env = 'development';
 const config = require('./knexfile.js')[env];
 const knex = require('knex')(config);
+
+const express = require('express');
+const app = express();
+
+app.listen(2000, function(){
+  console.log('Listening at port 2000...');
+});
